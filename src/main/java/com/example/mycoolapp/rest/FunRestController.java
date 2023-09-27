21d915be5b6ba2a5e7977686af302b0cccf8f9a6
Marien Mupenda <marien.mupenda@gmail.com>
2023-09-27 22:23:33 +0200

    add devtools

--- src/main/java/com/example/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/example/mycoolapp/rest/FunRestController.java
@@ -8,6 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class FunRestController {
     @GetMapping("/")
     public String sayHello() {
-        return "Hello World! Time on server is. ";
+        return "Hello World! Time on server is.";
+    }
+
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
+    }
+
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is your lucky day!";
     }
 }
