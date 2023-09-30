f91ca64058b31a0690955ec2fcc23b8cb02e5772
Marien Mupenda <marien.mupenda@gmail.com>
2023-09-30 22:26:55 +0200

    custom components

--- src/main/java/com/example/mycoolapp/MycoolappApplication.java
+++ src/main/java/com/example/mycoolapp/MycoolappApplication.java
@@ -3,6 +3,10 @@ package com.example.mycoolapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*@SpringBootApplication(
+        scanBasePackages = {"com.example.util", "com.example.mycoolapp"}
+)*/
+
 @SpringBootApplication
 public class MycoolappApplication {
 
--- src/main/java/com/example/mycoolapp/rest/DemoController.java
+++ src/main/java/com/example/mycoolapp/rest/DemoController.java
@@ -1,6 +1,6 @@
 package com.example.mycoolapp.rest;
 
-import com.example.mycoolapp.Coach;
+import com.example.mycoolapp.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
