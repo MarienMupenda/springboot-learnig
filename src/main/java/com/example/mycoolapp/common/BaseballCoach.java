14c973e4513924f60ec37aae463505047167e248
Marien Mupenda <marien.mupenda@gmail.com>
2023-09-30 22:51:22 +0200

    qualifier

---
+++ src/main/java/com/example/mycoolapp/common/BaseballCoach.java
@@ -0,0 +1,11 @@
+package com.example.mycoolapp.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BaseballCoach implements Coach {
+    @Override
+    public String getDailyWorkout() {
+        return "Spend 30 minutes on batting practice";
+    }
+}
--- src/main/java/com/example/mycoolapp/rest/DemoController.java
+++ src/main/java/com/example/mycoolapp/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.example.mycoolapp.rest;
 
 import com.example.mycoolapp.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,7 @@ public class DemoController {
 
 
     @Autowired
-    public void setCoach(Coach theCoach) {
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
         myCoach = theCoach;
     }
 
